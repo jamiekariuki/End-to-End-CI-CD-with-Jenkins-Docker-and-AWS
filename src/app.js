d8f10191320aa4c1a438619b36929f9af522cabb
kar!s <jamiekariuki18@gmail.com>
2025-06-30 18:03:24 +0300

    class demo

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 document.querySelector("h1").addEventListener("click", () => {
 	const greetings = [
-		"Welcome to the DevOps end to end project!",
+		"Welcome to the DevOps end to end project! resatrt",
 		"Automate everything with jenkins🚀",
 		"CI/CD makes life easier!",
 		"Docker + Jenkins = ❤️",

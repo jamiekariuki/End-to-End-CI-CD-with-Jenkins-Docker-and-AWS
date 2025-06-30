7e656aaa32008dc2e2fd4ac6e49fc57cf4c6d3b8
kar!s <jamiekariuki18@gmail.com>
2025-06-30 18:34:48 +0300

    update 2

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 document.querySelector("h1").addEventListener("click", () => {
 	const greetings = [
 		"Welcome guys we did it",
-		"Automate everything with jenkins🚀",
-		"CI/CD makes life easier!",
-		"Docker + Jenkins = ❤️",
-		"Enjoy your DevOps journey!",
+		
 	];
 	const h1 = document.querySelector("h1");
 	const randomGreeting =
